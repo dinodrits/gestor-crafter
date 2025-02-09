e72dbd1401a397072ca475f9b9db1a58c1699c32
dinodrits <diegobessa@gmail.com>
2025-02-09 03:20:39 -0300

    usina resource

---
+++ src/main/java/org/dino/resource/UsinaResource.java
@@ -0,0 +1,87 @@
+package org.dino.resource;
+
+import java.util.List;
+
+import org.dino.model.Usina;
+
+import jakarta.transaction.Transactional;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.DELETE;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.NotFoundException;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("/usina")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class UsinaResource {
+
+	@GET
+    public List<Usina> list() {
+        return Usina.listAll();
+    }
+
+//	@GET
+//    @Path("contratos/{id}")
+//    public List<Contrato> getContratos(Long id) {
+//		return  Contrato.find("idCliente.id = :id",
+//		         Parameters.with("id", id)).list();
+//        
+//    }
+	
+    @GET
+    @Path("/{id}")
+    public Usina get(Long id) {
+        return Usina.findById(id);
+    }
+
+    @POST
+    @Transactional
+    public Usina create(Usina usina) {
+    	System.out.println(usina);
+    	usina.persist();
+        return usina;
+    }
+
+    @PUT
+    @Path("/{id}")
+    @Transactional
+    public Usina update(Long id, Usina person) {
+    	Usina entity = Usina.findById(id);
+        if(entity == null) {
+            throw new NotFoundException();
+        }
+        
+        entity.setNome(person.getNome());
+        entity.setCidade(person.getCidade());
+        entity.setCapacidadeProducao(person.getCapacidadeProducao());
+        entity.setDataCriacao(person.getDataCriacao());
+        entity.setPotencia(person.getPotencia());
+        entity.setContato(person.getContato());
+        entity.setCpfCnpj(person.getCpfCnpj());
+        entity.setEmail(person.getEmail());
+        entity.setEndereco(person.getEndereco());
+        entity.setUf(person.getUf());
+        
+        entity.persist();
+        System.out.println(entity.getId());
+        
+        return entity;
+    }
+
+    @DELETE
+    @Path("/{id}")
+    @Transactional
+    public void delete(Long id) {
+        Usina entity = Usina.findById(id);
+        if(entity == null) {
+            throw new NotFoundException();
+        }
+        entity.delete();
+    }
+
+}
